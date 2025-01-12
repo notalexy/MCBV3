5dbe56633f014086b074cbb0cd087ac9ea8cd066
aryang535 <yangarnold535@gmail.com>
2025-01-12 16:54:20 -0500

    I'm the best documenter known to documenting kind

--- MCB-project/src/subsystems/flywheel/FlywheelSubsystem.cpp
+++ MCB-project/src/subsystems/flywheel/FlywheelSubsystem.cpp
@@ -5,11 +5,17 @@ namespace subsystems {
       : tap::control::Subsystem(drivers),
         drivers(drivers) {} 
 
+        /**
+         * initializes the 2 flywheels
+         */
     void FlyWheelSubsystem::initialize() {
         motor_Flywheel1.initialize();
         motor_Flywheel2.initialize();
     }
-
+    
+    /**
+     * sets the target velocity of the flywheels
+     */
     void FlyWheelSubsystem::setTargetVelocity(int targetVelocity){
         this->targetVelocity = targetVelocity;
         flywheelPIDController1.runControllerDerivateError(targetVelocity - motor_Flywheel1.getShaftRPM(), 1);
@@ -19,11 +25,17 @@ namespace subsystems {
         flyWheel2Voltage = static_cast<int32_t>(flywheelPIDController2.getOutput());
     }
 
+    /**
+     * refreshes flywheel output
+     */
     void FlyWheelSubsystem::refresh() {
         motor_Flywheel1.setDesiredOutput(flyWheel1Voltage);
         motor_Flywheel2.setDesiredOutput(flyWheel2Voltage);
     }
 
+    /**
+     * turn off
+     */
     void FlyWheelSubsystem::stopMotors() {
         flyWheel1Voltage = 0;
         flyWheel2Voltage = 0;
