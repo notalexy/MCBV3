beb7aa9e8eb84daf1f428341afbf5aeb5963344a
aryang535 <yangarnold535@gmail.com>
2025-01-12 15:34:46 -0500

    fart

--- MCB-project/src/subsystems/flywheel/FlywheelSubsystem.cpp
+++ MCB-project/src/subsystems/flywheel/FlywheelSubsystem.cpp
@@ -1,41 +1,38 @@
 #include "FlywheelSubsystem.h"
 
-namespace ThornBots {
-    ShooterController::ShooterController(tap::Drivers* driver) {
-        this->drivers = driver;
-        //TODO: Complete this
-    }
+namespace subsystems {
+    FlyWheelSubsystem::FlyWheelSubsystem(tap::Drivers* drivers) 
+      : tap::control::Subsystem(drivers),
+        drivers(drivers) {} 
+
     void ShooterController::initialize() {
-        motor_Indexer.initialize();
         motor_Flywheel1.initialize();
         motor_Flywheel2.initialize();
-        drivers->pwm.init(); //For the servo we will be using
-
-        //Nothing needs to be done to drivers
-        //Nothing needs to be done to the controllers
     }
-    void ShooterController::updateSpeeds(){
+    void FlyWheelSubsystem::updateSpeeds(){
         if(shooterControllerTimer.execute()) {
-            indexerVoltage = getIndexerVoltage();
+            // indexerVoltage = getIndexerVoltage();
             flyWheelVoltage = getFlywheelVoltage();
         }
     }
 
-    void ShooterController::setMotorSpeeds() {
+    void FlyWheelSubsystem::setMotorSpeeds() {
         updateSpeeds();
-        indexPIDController.runControllerDerivateError(indexerVoltage - motor_Indexer.getShaftRPM(), 1);
-        motor_Indexer.setDesiredOutput(static_cast<int32_t>(indexPIDController.getOutput()));
+        // indexPIDController.runControllerDerivateError(indexerVoltage - motor_Indexer.getShaftRPM(), 1);
+        // motor_Indexer.setDesiredOutput(static_cast<int32_t>(indexPIDController.getOutput()));
 
         flywheelPIDController1.runControllerDerivateError(flyWheelVoltage - motor_Flywheel1.getShaftRPM(), 1);
         motor_Flywheel1.setDesiredOutput(static_cast<int32_t>(flywheelPIDController1.getOutput()));
 
         flywheelPIDController2.runControllerDerivateError(flyWheelVoltage - motor_Flywheel2.getShaftRPM(), 1);
         motor_Flywheel2.setDesiredOutput(static_cast<int32_t>(flywheelPIDController2.getOutput()));
+
+        if(servoTimer.execute()) hopperServo.updateSendPwmRamp();
     }
 
-    void ShooterController::stopMotors() {
+    void FlyWheelSubsystem::stopMotors() {
         //indexPIDController.runControllerDerivateError(0 - motor_Indexer.getShaftRPM(), 1);
-        motor_Indexer.setDesiredOutput(0);//static_cast<int32_t>(indexPIDController.getOutput()));
+        // motor_Indexer.setDesiredOutput(0);//static_cast<int32_t>(indexPIDController.getOutput()));
 
        // flywheelPIDController1.runControllerDerivateError(0 - motor_Flywheel1.getShaftRPM(), 1);
         motor_Flywheel1.setDesiredOutput(0);//static_cast<int32_t>(flywheelPIDController1.getOutput()));
@@ -47,15 +44,15 @@ namespace ThornBots {
         //TODO: Add the other motors
     }
 
-    void ShooterController::enableShooting() {
+    void FlyWheelSubsystem::enableShooting() {
         this->shootingSafety = true;
     }
 
-    void ShooterController::disableShooting() {
+    void FlyWheelSubsystem::disableShooting() {
         this->shootingSafety = false;
     }
 
-    int ShooterController::getFlywheelVoltage() {
+    int FlyWheelSubsystem::getFlywheelVoltage() {
         if (robotDisabled) return 0;
         if(shootingSafety){
             return FLYWHEEL_MOTOR_MAX_SPEED;
@@ -64,25 +61,40 @@ namespace ThornBots {
         }
     }
 
-    int ShooterController::getIndexerVoltage() {
-        if (robotDisabled) return 0;
-        if(shootingSafety){
-            return indexerVoltage;
-        }else{
-            return 0;
-        }
-    }
+    // int FlyWheelSubsystem::getIndexerVoltage() {
+    //     if (robotDisabled) return 0;
+    //     if(shootingSafety){
+    //         return indexerVoltage;
+    //     }else{
+    //         return 0;
+    //     }
+    // }
 
-    void ShooterController::setIndexer(double val) {
-        indexerVoltage = val*INDEXER_MOTOR_MAX_SPEED;
-        
-    }
+    // void FlyWheelSubsystem::setIndexer(double val) {
+    //     indexerVoltage = val*INDEXER_MOTOR_MAX_SPEED;
+    // }
 
-    void ShooterController::disable(){
+    void FlyWheelSubsystem::disable(){
         robotDisabled = true;
     }
-    void ShooterController::enable(){
+    void FlyWheelSubsystem::enable(){
         robotDisabled = false;
     }
+    
+    void FlyWheelSubsystem::shoot(double maxFrequency) {
+        enableShooting();
+        closeServo();  // when shooting resumes close servo to prevent balls from leaving
+        tap::communication::serial::RefSerial::Rx::TurretData turretData = drivers->refSerial.getRobotData().turret;
 
-}
+        double latency = 0.4, remaining = turretData.heatLimit - turretData.heat17ID1;
+        // Check if the firing rate should be limited
+        if (drivers->refSerial.getRefSerialReceivingData() && (10.0 * maxFrequency - turretData.coolingRate) * latency > remaining) {
+            // Set the firing speed to C/10 Hz
+            maxFrequency = turretData.coolingRate / 10.0;
+        }
+
+        setIndexer(maxFrequency / 20.0);
+    }
+
+    void FlyWheelSubsystem::setServo(float val) { if(secondTimer.execute()) hopperServo.setTargetPwm(val); }
+}  // namespace subsystems
