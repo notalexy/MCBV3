ab2db5948bd65d4adfabc327cc651d6c3860cf12
aryang535 <yangarnold535@gmail.com>
2025-01-12 17:31:34 -0500

    indexer (doesn't work)

---
+++ MCB-project/src/subsystems/Indexer/Indexer.cpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/board/board.hpp"
+#include "tap/motor/dji_motor.hpp"
+#include "tap/motor/servo.hpp"
+
+#include "drivers_singleton.hpp"
+
+namespace subsystems {
+    static tap::arch::PeriodicMilliTimer shooterControllerTimer(2);
+    static tap::arch::PeriodicMilliTimer secondTimer(100);
+    static tap::arch::PeriodicMilliTimer servoTimer(20);
+
+    class Indexer {
+        public:  // Public Variables
+            // constexpr static double PI = 3.14159;
+            constexpr static int INDEXER_MOTOR_MAX_SPEED = 6177;   // With the 2006, this should give 
+
+        private:  // Private Variables
+            tap::Drivers* drivers;
+            // TODO: Check all motor ID's, and verify indexers and flywheels are in the correct direction
+            tap::motor::DjiMotor motor_Indexer =
+                tap::motor::DjiMotor(drivers::DoNotUse_getDrivers(), tap::motor::MotorId::MOTOR7, tap::can::CanBus::CAN_BUS2, false, "Indexer", 0, 0);
+
+            tap::algorithms::SmoothPid indexPIDController = tap::algorithms::SmoothPid(pid_conf_index);
+
+            double flyWheelVoltage, indexerVoltage = 0.0;
+
+        public:  // Public Methods
+        
+        void setIndexer(double val);
+        
+        inline void idle() { setIndexer(0); }
+        inline void unjam() { setIndexer(-0.1); }
+
+        private:  // Private Methods
+            int getFlywheelVoltage();
+            int getIndexerVoltage();
+            int getServoPosition();
+    };
+} //namespace subsystems
