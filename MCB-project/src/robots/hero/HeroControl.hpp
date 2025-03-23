1240a9a0cc271c4d5b460182f9778e66f9cfe36a
anthonaston_muiggin <wigginaston@gmail.com>
2025-03-23 18:08:31 -0400

    Added hero control and hardware files, need to ge t motor names for hero

---
+++ MCB-project/src/robots/hero/HeroControl.hpp
@@ -0,0 +1,126 @@
+#include "robots/RobotControl.hpp"
+
+#include "robots/hero/HeroHardware.hpp"
+
+#include "subsystems/ui/UISubsystem.hpp"
+#include "subsystems/ui/UIDrawCommand.hpp"
+
+#include "subsystems/gimbal/JoystickMoveCommand.hpp"
+#include "subsystems/gimbal/MouseMoveCommand.hpp"
+
+#include "subsystems/drivetrain/DrivetrainDriveCommand.hpp"
+#include "subsystems/drivetrain/DrivetrainStopCommand.hpp"
+#include "subsystems/flywheel/ShooterStartCommand.hpp"
+#include "subsystems/flywheel/ShooterStopCommand.hpp"
+#include "subsystems/gimbal/JoystickMoveCommand.hpp"
+#include "subsystems/gimbal/MouseMoveCommand.hpp"
+#include "subsystems/indexer/IndexerNBallsCommand.hpp"
+#include "subsystems/indexer/IndexerUnjamCommand.hpp"
+#include "subsystems/indexer/IndexerStopCommand.hpp"
+#include "subsystems/ui/UISubsystem.hpp"
+#include "util/trigger.hpp"
+
+#include "drivers.hpp"
+
+namespace robots {
+class HeroControl : public ControlInterface {
+public:
+    // pass drivers back to root robotcontrol to store
+    HeroControl(src::Drivers *drivers) : drivers(drivers), hardware(HeroHardware{drivers}) {}
+    // functions we are using
+    void initialize() override {
+        // Initialize subsystems
+        gimbal.initialize();
+        flywheel.initialize();
+        indexer.initialize();
+        drivetrain.initialize();
+        ui.initialize();
+
+        // Register subsystems;
+        drivers->commandScheduler.registerSubsystem(&gimbal);
+        drivers->commandScheduler.registerSubsystem(&flywheel);
+        drivers->commandScheduler.registerSubsystem(&indexer);
+        drivers->commandScheduler.registerSubsystem(&drivetrain);
+        drivers->commandScheduler.registerSubsystem(&ui);
+
+        // Run startup commands
+        gimbal.setDefaultCommand(&look);
+        flywheel.setDefaultCommand(&shooterStop);
+        indexer.setDefaultCommand(&indexerStopCommand);
+        
+
+        shootButton.whileTrue(&indexer10Hz)->onTrue(&shooterStart);
+        unjamButton.whileTrue(&indexerUnjam)->onTrue(&shooterStop);
+
+
+        // //peeking
+        peekLeftButton.whileTrue(&peekLeft);
+        peekRightButton.whileTrue(&peekRight);
+        
+        // drivers->commandMapper.addMap(&controllerToKeyboardMouseMapping);
+        // drivers->commandScheduler.addCommand(&indexerUnjam);
+    }
+
+    void update() override {
+
+        for (Trigger* trigger : triggers) {
+            trigger->update();
+        }
+    }
+
+    src::Drivers *drivers;
+    HeroHardware hardware;
+
+    // Subsystems
+    subsystems::UISubsystem ui{drivers};
+    subsystems::GimbalSubsystem gimbal{drivers, &hardware.yawMotor, &hardware.pitchMotor};
+    subsystems::FlywheelSubsystem flywheel{drivers, &hardware.flywheelMotor1, &hardware.flywheelMotor2};
+    subsystems::IndexerSubsystem indexer{drivers, &hardware.indexMotor};
+    subsystems::DrivetrainSubsystem drivetrain{drivers, &hardware.driveMotor1, &hardware.driveMotor2, &hardware.driveMotor3, &hardware.driveMotor4};
+
+    // //commands
+    commands::UIDrawCommand draw{&ui};
+
+    commands::JoystickMoveCommand look{drivers, &gimbal};
+    commands::MouseMoveCommand look2{drivers, &gimbal};
+
+    commands::ShooterStartCommand shooterStart{drivers, &flywheel};
+    commands::ShooterStopCommand shooterStop{drivers, &flywheel};
+
+    commands::IndexerNBallsCommand indexer10Hz{drivers, &indexer, -1, 10};
+    commands::IndexerUnjamCommand indexerUnjam{drivers, &indexer};
+    commands::IndexerStopCommand indexerStopCommand{drivers, &indexer};
+
+    //CHANGE NUMBERS LATER
+    commands::DrivetrainDriveCommand peekRight{drivers, &drivetrain, &gimbal, commands::DriveMode::PEEK_RIGHT, commands::ControlMode::KEYBOARD};
+    commands::DrivetrainDriveCommand peekLeft{drivers, &drivetrain, &gimbal, commands::DriveMode::PEEK_LEFT, commands::ControlMode::KEYBOARD};
+    commands::DrivetrainDriveCommand peekCenterWASD{drivers, &drivetrain, &gimbal, commands::DriveMode::FOLLOW_TURRET, commands::ControlMode::KEYBOARD};
+    commands::DrivetrainDriveCommand peekCenterJoystick{drivers, &drivetrain, &gimbal, commands::DriveMode::FOLLOW_TURRET, commands::ControlMode::CONTROLLER};
+
+    commands::DrivetrainStopCommand stopDriveCommand{drivers, &drivetrain};
+
+    // mappings
+
+
+    //maybe change later
+
+    //peeking
+    Trigger peekLeftButton{drivers, Remote::Key::Q};
+    Trigger peekRightButton{drivers, Remote::Key::E};
+    Trigger peekCenterButton{drivers, Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP};// = (Trigger(drivers, Remote::Key::Q) & Trigger(drivers, Remote::Key::E)) | Trigger(drivers, Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP);
+
+    //shooting
+    //shooting
+    Trigger shootButton{drivers, Remote::Channel::WHEEL, 0.5};// = Trigger(drivers, Remote::Channel::WHEEL, 0.5) | Trigger(drivers, MouseButton::LEFT);
+    //unjamming we know the button reacts 
+    Trigger unjamButton{drivers, Remote::Channel::WHEEL, -0.5};// = Trigger(drivers, Remote::Channel::WHEEL, -0.5) | Trigger(drivers, MouseButton::RIGHT);
+
+    //Trigger indexSpinButton;// = Trigger(drivers, Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP) & Trigger(drivers, Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP);
+    Trigger* triggers[5] = {&peekLeftButton, &peekRightButton, &peekCenterButton, &shootButton, &unjamButton};//, &indexSpinButton};
+
+
+    // ToggleCommandMapping controllerToKeyboardMouseMapping{drivers, {&look2}, RemoteMapState({Remote::Key::CTRL, Remote::Key::Z})};
+
+};
+
+}  // namespace robots
---
+++ MCB-project/src/robots/hero/HeroHardware.hpp
@@ -0,0 +1,43 @@
+#include "tap/architecture/periodic_timer.hpp"
+#include "tap/board/board.hpp"
+#include "tap/motor/dji_motor.hpp"
+#include "tap/motor/servo.hpp"
+
+#include "subsystems/gimbal/GimbalSubsystem.hpp"
+#include "subsystems/flywheel/FlywheelSubsystem.hpp"
+#include "subsystems/indexer/IndexerSubsystem.hpp"
+#include "subsystems/drivetrain/DrivetrainSubsystem.hpp"
+
+#include "drivers.hpp"
+
+using namespace tap::motor;
+using namespace tap::can;
+
+namespace robots
+{
+//class for standard robot. This has hardware and subsystems that are robot specific. This means we can have multiple StandardControl for different control schemes
+class HeroHardware
+{
+public:
+    HeroHardware(src::Drivers* drivers) : drivers(drivers) {}
+
+    //drivers
+    src::Drivers* drivers;
+
+    //motors 
+    DjiMotor flywheelMotor1{drivers, MotorId::MOTOR5, CanBus::CAN_BUS2, true, "Flywheel", 0, 0};
+    DjiMotor flywheelMotor2{drivers, MotorId::MOTOR8, CanBus::CAN_BUS2, false, "Flywheel", 0, 0};
+
+    DjiMotor yawMotor{drivers, MotorId::MOTOR7, CanBus::CAN_BUS1, false, "Yaw", 0, 0};
+    DjiMotor pitchMotor{drivers, MotorId::MOTOR6, CanBus::CAN_BUS2, false, "Pitch", 0, 0};
+
+    DjiMotor indexMotor{drivers, MotorId::MOTOR7, CanBus::CAN_BUS2, false, "Indexer2", 0, 0};
+
+    DjiMotor driveMotor1{drivers, MotorId::MOTOR1, CanBus::CAN_BUS1, true, "Motor 1", 0, 0};
+    DjiMotor driveMotor2{drivers, MotorId::MOTOR3, CanBus::CAN_BUS1, true, "Motor 2", 0, 0};
+    DjiMotor driveMotor3{drivers, MotorId::MOTOR4, CanBus::CAN_BUS1, true, "Motor 3", 0, 0};
+    DjiMotor driveMotor4{drivers, MotorId::MOTOR2, CanBus::CAN_BUS1, true, "Motor 4", 0, 0};
+
+};
+
+}  // namespace ThornBots
